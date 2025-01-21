97271de6216f82bdbb2462a3a797eb3bcd3e12f8
Sateesh2004 <kumarsateesh838@gmail.com>
2025-01-22 00:27:55 +0530

    "Footer Removed"

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -26,15 +26,7 @@ export default function RootLayout({ children }) {
       >
         <NavBar/>
         {children}
-        <footer className="bg-gray-800 text-white py-8">
-        <div className="container mx-auto px-4 text-center">
-          <p>&copy; Made by 
-Kunal Sharma | 
-Lawkesh Kumar | 
-Laloo Sahani | 
-Mayur Yadhuvanshi</p>
-        </div>
-      </footer>
+        
       </body>
     </html>
   );
